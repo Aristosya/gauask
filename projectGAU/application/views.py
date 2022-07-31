005ae3bccf1f6fed611b1d90666a84d2542fbc9c
Roman Alexandrovich <87494379+Aristosya@users.noreply.github.com>
2022-07-31 19:53:55 +0300

    remove try

--- projectGAU/application/views.py
+++ projectGAU/application/views.py
@@ -48,26 +48,22 @@ def createTeacher(request):
             first_name = request.POST['name']
             last_name = request.POST['surname']
             email = request.POST['email']
-            try:
-                msg = EmailMessage()
-                msg['Subject'] = 'GauAsk Password'
-                msg['From'] = 'admin@gauask.com'
-                msg['To'] = email
-                msg.set_content('Your password is TeacherGauAsk2022*')
-                server = smtplib.SMTP_SSL('smtp-muhammed123123.alwaysdata.net', 465)
-                server.login('admin@gauask.com', 'GL>32yAV')
-                server.send_message(msg)
-                server.quit()
+            msg = EmailMessage()
+            msg['Subject'] = 'GauAsk Password'
+            msg['From'] = 'admin@gauask.com'
+            msg['To'] = email
+            msg.set_content('Your password is TeacherGauAsk2022*')
+            server = smtplib.SMTP_SSL('smtp-muhammed123123.alwaysdata.net', 465)
+            server.login('admin@gauask.com', 'GL>32yAV')
+            server.send_message(msg)
+            server.quit()
                 # create a user with email and password that we generate
-                User.objects.create_user(username=email, password='TeacherGauAsk2022*', email=email, first_name=first_name,
-                                         last_name=last_name)
-                newuser = User.objects.filter(username=email).first()
-                newuser.profile.teacher = True
-                newuser.save()
+            User.objects.create_user(username=email, password='TeacherGauAsk2022*', email=email, first_name=first_name,last_name=last_name)
+            newuser = User.objects.filter(username=email).first()
+            newuser.profile.teacher = True
+            newuser.save()
                 # ok massage for home page
-                messages.success(request, f'Password has been sent to email')
-            except:
-                pass
+            messages.success(request, f'Password has been sent to email')
         return render(request, 'application/create_teacher.html')
     return redirect('home')
 
